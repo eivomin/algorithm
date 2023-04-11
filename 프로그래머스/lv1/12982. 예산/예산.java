a831edee9f617a6eb1c12b95db053419c3d54b68
eivomin <97949070+eivomin@users.noreply.github.com>
2023-04-11 13:30:35 +0900

    [level 1] Title: 예산, Time: 0.34 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        int cnt = 0;
+        int sum = 0;
+        for(int i=0; i<d.length; i++){
+            sum += d[i];
+            if(sum > budget) break;
+            cnt++;
+        }
+        return cnt;
+    }
+}
