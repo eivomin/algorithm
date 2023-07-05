141a99af008ff8460faacecd6448b02568a656f0
eivomin <97949070+eivomin@users.noreply.github.com>
2023-07-05 14:56:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.14 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        
-        for(String s : participant){
-            if(hashMap.get(s) == null)
-                hashMap.put(s, 1);
-            else
-                hashMap.put(s, hashMap.get(s)+1);
-        }
-        
-        for(String s : completion){
-            if(hashMap.get(s) != 0)
-                hashMap.put(s, hashMap.get(s)-1);
-        }
-        
-        for(String key : hashMap.keySet()){
-            if(hashMap.get(key) != 0)
-                return key;
-        }
         return answer;
     }
 }
