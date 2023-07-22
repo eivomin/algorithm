f44c786462a77c4c7b3abf86219d32675e75f960
eivomin <97949070+eivomin@users.noreply.github.com>
2023-07-22 14:05:38 +0900

    [Silver III] Title: N과 M (7), Time: 788 ms, Memory: 266116 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    //정렬 후 중복순열
+    static int N, M;
+    static int[] arr, res;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[N];
+        res = new int[M];
+        sb = new StringBuilder();
+
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        sort_perm_duplicate(0);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void sort_perm_duplicate(int idx) {
+        if (idx == M) {
+            for(int a : res)
+                sb.append(a+" ");
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<N; i++){
+            res[idx] = arr[i];
+            sort_perm_duplicate(idx+1);
+        }
+    }
+}
