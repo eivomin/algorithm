5cd39acd5b920b6ee7be65ee3b73c0457464eef6
eivomin <97949070+eivomin@users.noreply.github.com>
2023-07-26 20:17:50 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1708 ms, Memory: 103936 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<Coordinate> list = new ArrayList<>();
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            list.add(new Coordinate(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        Comparator<Coordinate> compare = Comparator
+                .comparing(Coordinate::getY)
+                .thenComparing(Coordinate::getX);
+
+        list.stream()
+                .sorted(compare)
+                .forEach(coordinate -> System.out.println(coordinate.x+" "+coordinate.y));
+    }
+
+    static class Coordinate{
+        int x, y;
+        public Coordinate(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+}
