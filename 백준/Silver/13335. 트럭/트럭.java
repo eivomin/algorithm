faf6b5130545f963b5244021aa71fdc1e1a990c4
eivomin <97949070+eivomin@users.noreply.github.com>
2023-07-28 16:33:11 +0900

    [Silver I] Title: 트럭, Time: 152 ms, Memory: 15268 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        Queue<Integer> truck = new LinkedList<>();
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<n; i++)
+            truck.add(Integer.parseInt(st.nextToken()));
+
+        int time = 0;
+        int bw = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<w; i++)
+            queue.add(0);
+
+        while(!queue.isEmpty()){
+            time++;
+            bw -= queue.poll();
+            if(!truck.isEmpty()){
+                if(truck.peek()+bw <= L){
+                    bw += truck.peek();
+                    queue.add(truck.poll());
+                }else{
+                    queue.add(0);
+                }
+            }
+        }
+        System.out.println(time);
+    }
+}
