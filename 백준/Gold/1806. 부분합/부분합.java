dd793b723549133a672715012d6a89942728ed25
eivomin <97949070+eivomin@users.noreply.github.com>
2023-08-08 13:00:28 +0900

    [Gold IV] Title: 부분합, Time: 292 ms, Memory: 23884 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N+1];
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i=0; i<N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        while(start <= N && end <= N){
+            if(sum >= S && min > end - start) min = end-start;
+            if(sum < S) sum += arr[end++];
+            else sum -= arr[start++];
+        }
+
+        if(min == Integer.MAX_VALUE)
+            System.out.println(0);
+        else System.out.println(min);
+
+    }
+}
