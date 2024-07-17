553fc630d0a5fef39b6c33e375375fc841b8f25e
eivomin <97949070+eivomin@users.noreply.github.com>
2024-07-17 21:24:05 +0900

    [Silver IV] Title: 스택, Time: 336 ms, Memory: 18832 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,38 +1,44 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<N; i++){
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            String cmd = st.nextToken();
-            if(cmd.equals("push")){
-                int a = Integer.parseInt(st.nextToken());
-                stack.push(a);
-            }else if(cmd.equals("top")){
-                if(!stack.isEmpty())
-                    sb.append(stack.peek()+"\n");
-                else sb.append(-1+"\n");
-            }else if(cmd.equals("pop")){
-                if(!stack.isEmpty())
-                  sb.append(stack.pop()+"\n");
-                else sb.append(-1+"\n");
-            }else if(cmd.equals("size")){
-                sb.append(stack.size()+"\n");
-            }else if(cmd.equals("empty")){
-                if(stack.isEmpty())
-                    sb.append(1+"\n");
-                else sb.append(0+"\n");
+            String command = st.nextToken();
+
+            if (command.equals("push")) {
+                stack.push(Integer.parseInt(st.nextToken()));
+            } else if (command.equals("top")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.peek());
+                }
+            } else if (command.equals("size")) {
+                System.out.println(stack.size());
+            } else if (command.equals("empty")) {
+                if (stack.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            } else if (command.equals("pop")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.pop());
+                }
             }
         }
-
-        System.out.println(sb.toString());
-        br.close();
     }
 }
